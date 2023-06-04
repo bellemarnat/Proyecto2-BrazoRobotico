3e7d41144d4438cbe99a21c3db662c33032471da
Belle Marnat <isabelle.marnat2002@gmail.com>
2023-06-03 18:45:39 -0600

    codigo funcional traducido a puertos

---
+++ emisor_semifinal_mejorado/emisor_semifinal_mejorado.ino
@@ -0,0 +1,49 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(4, 5); // RX, TX
+
+#define JOY1_X 0 // Pin A0
+#define JOY1_Y 1 // Pin A1
+#define JOY2_X 2 // Pin A2
+#define JOY2_Y 3 // Pin A3
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600); // Iniciar mySerial
+
+  // Configurar el ADC
+  ADMUX = (1 << REFS0); // use AVcc as the reference
+  ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Habilitar el ADC configurando el preescalador a 128
+}
+
+void loop() {
+  int joy1_x = readADC(JOY1_X);
+  int joy1_y = readADC(JOY1_Y);
+  int joy2_x = readADC(JOY2_X);
+  int joy2_y = readADC(JOY2_Y);
+
+  Serial.print("Joy1_X: ");
+  Serial.print(joy1_x);
+  Serial.print(" Joy1_Y: ");
+  Serial.print(joy1_y);
+  Serial.print(" Joy2_X: ");
+  Serial.print(joy2_x);
+  Serial.print(" Joy2_Y: ");
+  Serial.println(joy2_y);
+
+  // Enviar los valores a través de mySerial
+  mySerial.print(joy1_x);
+  mySerial.print(',');
+  mySerial.print(joy1_y);
+  mySerial.print(',');
+  mySerial.print(joy2_x);
+  mySerial.print(',');
+  mySerial.println(joy2_y); // Asegúrate de que los valores estén separados por una coma y que el último valor termine con un salto de línea
+}
+
+int readADC(int channel) {
+  ADMUX = (ADMUX & 0xF0) | (channel & 0x0F); // Seleccionar el canal del ADC
+  ADCSRA |= (1 << ADSC); // Iniciar la conversión
+  while (ADCSRA & (1 << ADSC)); // Esperar a que la conversión termine
+  return ADC; // Leer el valor de la conversión
+}
---
+++ receptor_semifinal_mejorado/receptor_semifinal_mejorado.ino
@@ -0,0 +1,96 @@
+#include <SoftwareSerial.h>
+#include <Servo.h>
+
+Servo Servo1;
+Servo Servo2;
+Servo Servo3;
+Servo Servo4;
+
+SoftwareSerial mySerial(4, 5); // RX, TX
+
+int val1, val2, val3, val4;
+int prev_val1, prev_val2, prev_val3, prev_val4;
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+
+  Servo1.attach(8);
+  Servo2.attach(9);
+  Servo3.attach(10);
+  Servo4.attach(11);
+  
+  prev_val1 = prev_val2 = prev_val3 = prev_val4 = 90; // Servo initial position
+}
+
+void loop() {
+  if (mySerial.available()) {
+    String input = mySerial.readStringUntil('\n');
+    Serial.print("Entrada recibida: ");
+    Serial.println(input); // Imprimir la entrada directamente
+
+    int index = 0;
+    int delimiterIndex;
+
+    while ((delimiterIndex = input.indexOf(',')) != -1) {
+      String value = input.substring(0, delimiterIndex);
+      input = input.substring(delimiterIndex + 1);
+      assignValue(index, value);
+      index++;
+    }
+    assignValue(index, input);
+
+    printValues();
+
+    smoothServoMove(); // Move servos in a smooth way
+  }
+}
+
+void assignValue(int index, String value) {
+  int val = value.toInt();
+  val = constrain(val, 0, 1023);  // Restringe los valores entre 0 y 1023
+
+  switch (index) {
+    case 0:
+      val1 = map(val, 0, 1023, 0, 180);  // Map to 0 - 180 degrees for Servo
+      break;
+    case 1:
+      val2 = map(val, 0, 1023, 0, 180);  // Map to 0 - 180 degrees for Servo
+      break;
+    case 2:
+      val3 = map(val, 0, 1023, 0, 180);  // Map to 0 - 180 degrees for Servo
+      break;
+    case 3:
+      val4 = map(val, 0, 1023, 0, 180);  // Map to 0 - 180 degrees for Servo
+      break;
+  }
+}
+
+void printValues() {
+  Serial.print("val1 = ");
+  Serial.print(val1);
+  Serial.print(", val2 = ");
+  Serial.print(val2);
+  Serial.print(", val3 = ");
+  Serial.print(val3);
+  Serial.print(", val4 = ");
+  Serial.println(val4);
+}
+
+void smoothServoMove() {
+  int steps = 100; // Define the number of steps
+  int delayTime = 10; // Delay between each step
+  
+  for (int i = 0; i <= steps; i++) {
+    Servo1.write(prev_val1 + ((val1 - prev_val1) * i / steps));
+    Servo2.write(prev_val2 + ((val2 - prev_val2) * i / steps));
+    Servo3.write(prev_val3 + ((val3 - prev_val3) * i / steps));
+    Servo4.write(prev_val4 + ((val4 - prev_val4) * i / steps));
+    delay(delayTime);
+  }
+  
+  prev_val1 = val1;
+  prev_val2 = val2;
+  prev_val3 = val3;
+  prev_val4 = val4;
+}
